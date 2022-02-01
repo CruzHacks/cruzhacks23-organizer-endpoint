f2cbd7863f40baaeee58a579e4b5e95078b3105a
Alperdec <49699829+Alperdec@users.noreply.github.com>
2022-01-31 19:14:00 -0800

    added schedule datas

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const helmet = require ('helmet');
+const helmet = require('helmet');
+const { schedule } = require('schedule.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,10 +12,11 @@ app.get('/', (req, res) => {
   res.status(200).send({
     error: false,
     status: 200,
-    message: "hello there!"
+    itemCount: 3,
+    payload: JSON.stringify(schedule)
   });
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}`);
+  console.log(`cruzhacks-organizer-challenge listening on port ${PORT}`);
 });
