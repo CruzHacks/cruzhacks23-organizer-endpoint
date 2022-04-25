6a49998e1a47110a94577a2cdc585b4053383d6e
Alperdec <49699829+Alperdec@users.noreply.github.com>
2022-04-25 15:00:52 -0700

    f

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const { schedule } = require('./schedule.js');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 app.disable('x-powered-by');
 app.use(helmet());
 app.use(cors({
   origin: '*'
-  }))
-;
+  })
+);
 
 const getRequestLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	max: 100,               // Limit each IP to 100 requests per `window` (here, per 15 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
